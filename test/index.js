20c69f2e379cd03d66c0c98d43f331d374b99120
Titus Wormer <tituswormer@gmail.com>
2023-09-23 10:35:25 +0200

    Refactor code-style

--- test/index.js
+++ test/index.js
@@ -1,70 +1,74 @@
 import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
 import test from 'node:test'
-import {unified} from 'unified'
+import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import remarkStringify from 'remark-stringify'
-import rehypeStringify from 'rehype-stringify'
+import {unified} from 'unified'
 import {VFile} from 'vfile'
 import remarkEmbedImages from '../index.js'
 
-test('remark-embed-images', async () => {
-  assert.match(
-    String(
-      await unified()
-        .use(remarkParse)
-        .use(remarkEmbedImages)
-        .use(remarkStringify)
-        .process('![](./test/fixtures/foo/foo.png)')
-    ),
-    /!\[]\(data:image\/png;base64,/,
-    'should inline images w/o file path'
-  )
+test('remark-embed-images', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+      'default'
+    ])
+  })
 
-  assert.match(
-    String(
-      await unified()
-        .use(remarkParse)
-        .use(remarkEmbedImages)
-        .use(remarkStringify)
-        .process('![](test/fixtures/foo/foo.png)')
-    ),
-    /!\[]\(data:image\/png;base64,/,
-    'should inline images w/o file path'
-  )
-
-  try {
-    await unified()
-      .use(remarkParse)
-      .use(remarkEmbedImages)
-      .use(remarkStringify)
-      .process('![Some missing image](./missing.png)')
-    assert.fail()
-  } catch (error) {
+  await t.test('should inline images w/o file path', async function () {
     assert.match(
-      String(error),
-      /no such file or directory/,
-      'should throw for missing files'
+      String(
+        await unified()
+          .use(remarkParse)
+          .use(remarkEmbedImages)
+          .use(remarkStringify)
+          .process('![](./test/fixtures/foo/foo.png)')
+      ),
+      /!\[]\(data:image\/png;base64,/
     )
-  }
+  })
 
-  const input = new URL('fixtures/foo/input.md', import.meta.url)
-  const output = new URL('foo-output.html', import.meta.url)
-  const expected = String(await fs.readFile(output))
-  const actual = String(
-    await unified()
-      .use(remarkParse)
-      .use(remarkEmbedImages)
-      .use(remarkRehype)
-      .use(rehypeStringify)
-      .process(new VFile({path: input, value: await fs.readFile(input)}))
-  )
-  assert.equal(
-    actual.trimEnd(),
-    expected.trimEnd(),
-    'should integrate with rehype'
-  )
+  await t.test('should inline images w/o file path', async function () {
+    assert.match(
+      String(
+        await unified()
+          .use(remarkParse)
+          .use(remarkEmbedImages)
+          .use(remarkStringify)
+          .process('![](test/fixtures/foo/foo.png)')
+      ),
+      /!\[]\(data:image\/png;base64,/
+    )
+  })
+
+  await t.test('should throw for missing files', async function () {
+    try {
+      await unified()
+        .use(remarkParse)
+        .use(remarkEmbedImages)
+        .use(remarkStringify)
+        .process('![Some missing image](./missing.png)')
+      assert.fail()
+    } catch (error) {
+      assert.match(String(error), /no such file or directory/)
+    }
+  })
+
+  await t.test('should integrate with rehype', async function () {
+    const input = new URL('fixtures/foo/input.md', import.meta.url)
+    const output = new URL('foo-output.html', import.meta.url)
+    const expected = String(await fs.readFile(output))
+    const actual = String(
+      await unified()
+        .use(remarkParse)
+        .use(remarkEmbedImages)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+        .process(new VFile({path: input, value: await fs.readFile(input)}))
+    )
+    assert.equal(actual.trimEnd(), expected.trimEnd())
+  })
 })
 
 test('fixtures', async function (t) {
